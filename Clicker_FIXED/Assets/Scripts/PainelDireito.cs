8cb914e087fc087c74247d2d5e47868e9b5cce73
DouglasKotsubo <dkslenderman@gmail.com>
2021-10-15 02:17:14 -0300

    PanielEsq

--- Clicker_FIXED/Assets/Scripts/PainelDireito.cs
+++ Clicker_FIXED/Assets/Scripts/PainelDireito.cs
@@ -5,15 +5,24 @@ using UnityEngine.UI;
 public class PainelDireito : MonoBehaviour
 {
     //ESSE SCRIPT É PRA ESCONDER/MOSTRAR O PAINEL DA DIREITA QUANDO APERTAR NO BOTÃO COM A SETINHA
-    int aberto = 1;
+    int abertoesq = 1;
+    int abertodir = 1; //F atribuições
     public float width;
     public RectTransform toggleButton;
-    public void AbreFecha(){
-        aberto = 1 - aberto;
+    public void AbreFechaDireito(){
+        abertodir = 1 - abertodir;
         toggleButton.localScale = new Vector3 (-toggleButton.localScale.x, toggleButton.localScale.y, toggleButton.localScale.z);
-        if (aberto == 0){
+        if (abertodir == 0){
             GetComponent<RectTransform>().localPosition += Vector3.right * width;
         }
         else GetComponent<RectTransform>().localPosition += Vector3.left * width;
+    }
+    public void AbreFechaEsquerdo(){
+        abertoesq = 1 - abertoesq;
+        toggleButton.localScale = new Vector3 (-toggleButton.localScale.x, toggleButton.localScale.y, toggleButton.localScale.z);
+        if (abertoesq == 0){
+            GetComponent<RectTransform>().localPosition += Vector3.left * width;
+        }
+        else GetComponent<RectTransform>().localPosition += Vector3.right * width;
     }
 }
