e4460864f15dd7f277407a7407e607c6fe298f3e
DouglasKotsubo <dkslenderman@gmail.com>
2021-10-15 02:07:20 -0300

    AudioManager

---
+++ Clicker_FIXED/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    
+    public Sound[] sounds;
+
+    public AudioManager instance; 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+
+            s.source.loop = s.loop;
+        }
+    }
+
+    // Update is called once per frame
+    void PlaySound(string name)
+    {
+        Sound s = Array.Find(sounds, sounds => sounds.name == name);
+        if (s == null)
+            return;
+        s.source.Play();
+    }
+}
