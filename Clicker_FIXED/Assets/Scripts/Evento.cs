7aae9d69f5e5fccd7502c7e67d4a5ce3e71b19a1
Atlaswe <galtmerklein@gmail.com>
2021-10-16 00:06:04 -0300

    Fiz o prefab da carta

---
+++ Clicker_FIXED/Assets/Scripts/Evento.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Evento
+{
+    public string mensagem;
+    public string remetente;
+    public int consequenciaIndex;
+}
---
+++ Clicker_FIXED/Assets/Scripts/LetterEvents.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LetterEvents : MonoBehaviour
+{
+    public GameObject carta;
+    public Evento[] allEvents;
+    private Money money;
+    private int consequenceIndex = 0;
+
+    [Header("GUI")]
+    public TextMeshProUGUI remetente;
+    public TextMeshProUGUI mensagem;
+    void Start(){
+        money = GameObject.Find("MoneyManager").GetComponent<Money>();
+        carta.SetActive(false);
+    }
+    
+    public void Consequencias(){
+        if (consequenceIndex == 1){ //80K
+            money.currency += 80000;
+            StartCoroutine(Desligamento());
+        }
+        else if (consequenceIndex == 2){
+            SceneManager.LoadScene("StartScene");
+        }
+        EndLetter(); 
+        //ADICIONAR MAIS EVENTOS AQUI
+    }
+
+    void StartLetter(Evento escolhido){
+        consequenceIndex = escolhido.consequenciaIndex;
+        remetente.text = escolhido.remetente;
+        mensagem.text = escolhido.mensagem;
+        carta.SetActive(true);
+        RemoveEvento(escolhido.consequenciaIndex);
+
+    }
+
+    private void RemoveEvento(int index){
+        foreach (Evento e in allEvents){
+            if (e.consequenciaIndex == index){
+                e.consequenciaIndex = 0;
+                e.mensagem = "";
+                e.remetente = "";
+            }
+        }
+    }
+
+    void EndLetter(){
+        if (consequenceIndex == 2) SceneManager.LoadScene("StartScene");
+        carta.SetActive(false);
+    }
+
+    void Update(){
+        if (money.currency > 1000){
+            if (allEvents[0].consequenciaIndex != 0) StartLetter(allEvents[0]);
+        }
+    }
+
+    IEnumerator Desligamento(){
+        yield return new WaitForSeconds(30);
+        StartLetter(allEvents[1]); //Evento de desligamento
+
+    }
+}
